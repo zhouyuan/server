zhouyuan/server

commit 0f01bf267680244ec488adaf65a42838756ed48e

MDEV-8241: Debug build on Windows is broken: error LNK2019: unresolved external symbol pthread_detach referenced in function ma_checkpoint_init
pthread_detach() replaced with pthread_detach_this_thread()

pthread_detach_this_thread() definition fixed

include/my_pthread.h
@@ -182,7 +182,7 @@ int pthread_cancel(pthread_t thread);
 #define pthread_key(T,V) pthread_key_t V
 #define my_pthread_getspecific_ptr(T,V) my_pthread_getspecific(T,(V))
 #define my_pthread_setspecific_ptr(T,V) pthread_setspecific(T,(void*) (V))
-#define pthread_detach_this_thread()
+#define pthread_detach_this_thread() { pthread_t tmp=pthread_self() ; pthread_detach(tmp); }
 #define pthread_handler_t EXTERNC void *
 typedef void *(* pthread_handler)(void *);
 

storage/maria/ma_checkpoint.c
@@ -340,7 +340,6 @@ int ma_checkpoint_init(ulong interval)
     {
       /* thread lives, will have to be killed */
       checkpoint_control.status= THREAD_RUNNING;
-      pthread_detach(th);
     }
   }
   DBUG_RETURN(res);
@@ -573,6 +572,8 @@ pthread_handler_t ma_checkpoint_background(void *arg)
   */
   sleeps= 1;
   pages_to_flush_before_next_checkpoint= 0;
+
+  pthread_detach_this_thread();
 
   for(;;) /* iterations of checkpoints and dirty page flushing */
   {

commit c45b4a774b6d1404a080a1c1759f780fa78f223b

MDEV-17200 - pthread_detach called for already detached threads
pthread_detach_this_thread() was intended to be defined to something
meaningful only on some ancient unixes, which don't have
pthread_attr_setdetachstate() defined. Otherwise, on normal unixes,
threads are created detached in the first place.

This was broken in 0f01bf267680244ec488adaf65a42838756ed48e so that
we started calling pthread_detach() for already detached threads.
Intention was to detach aria checkpoint thread.

However in 87007dc2f71634cc460271eb277ad851ec69c04b aria service threads
were made joinable with appropriate handling, which makes breaking
revision unneccessary.

Revert remnants of 0f01bf267680244ec488adaf65a42838756ed48e, so that
pthread_detach_this_thread() is meaningful only on some ancient unixes
again.

include/my_pthread.h
@@ -184,7 +184,7 @@ int pthread_cancel(pthread_t thread);
 #define pthread_key(T,V) pthread_key_t V
 #define my_pthread_getspecific_ptr(T,V) my_pthread_getspecific(T,(V))
 #define my_pthread_setspecific_ptr(T,V) pthread_setspecific(T,(void*) (V))
-#define pthread_detach_this_thread() { pthread_t tmp=pthread_self() ; pthread_detach(tmp); }
+#define pthread_detach_this_thread()
 #define pthread_handler_t EXTERNC void *
 typedef void *(* pthread_handler)(void *);
 